49449ad368f4afed0c7044442fe896bb1a7f6b39
Rachel <83382319+Super-Sage@users.noreply.github.com>
2022-06-10 13:15:58 -0400

    Solved object reference issue

--- src/Microsoft.PowerApps.TestEngine/TestInfra/PlaywrightTestInfraFunctions.cs
+++ src/Microsoft.PowerApps.TestEngine/TestInfra/PlaywrightTestInfraFunctions.cs
@@ -123,7 +123,7 @@ namespace Microsoft.PowerApps.TestEngine.TestInfra
             // TODO: consider whether to make waiting for network idle state part of the function input
             var response = await Page.GotoAsync(url, new PageGotoOptions() { WaitUntil = WaitUntilState.NetworkIdle });
 
-            if (!response.Ok)
+            if (response != null && !response.Ok )
             {
                 _singleTestInstanceState.GetLogger().LogError($"Error navigating to page: {url}, response is: {response?.Status}");
                 throw new InvalidOperationException("Go to url failed");
