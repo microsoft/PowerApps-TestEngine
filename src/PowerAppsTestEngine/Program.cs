611c9088190f6a14b24ee2a90156d46e18502b61
Landon Rogers <landonrogers@microsoft.com>
2022-06-16 12:19:52 -0400

    Simplify testing

--- src/PowerAppsTestEngine/Program.cs
+++ src/PowerAppsTestEngine/Program.cs
@@ -22,19 +22,6 @@ using var serviceProvider = new ServiceCollection()
             .AddConsole() // TODO: figure out why I can't have both console logging and the test logger at the same time.
             .AddProvider(new TestLoggerProvider(new FileSystem()));
         })
-    .AddScoped<ITestInfraFunctions, PlaywrightTestInfraFunctions>()
-    .AddSingleton<ITestConfigParser, YamlTestConfigParser>()
-    .AddScoped<IPowerFxEngine, PowerFxEngine>()
-    .AddScoped<IUserManager, UserManager>()
-    .AddSingleton<ITestState, TestState>()
-    .AddScoped<IUrlMapper, PowerAppsUrlMapper>()
-    .AddScoped<IPowerAppFunctions, PowerAppFunctions>()
-    .AddSingleton<ITestReporter, TestReporter>()
-    .AddScoped<ISingleTestInstanceState, SingleTestInstanceState>()
-    .AddScoped<ISingleTestRunner, SingleTestRunner>()
-    .AddSingleton<IFileSystem, FileSystem>()
-    .AddSingleton<IEnvironmentVariable, EnvironmentVariable>()
-    .AddSingleton<TestEngine>()
     .BuildServiceProvider();
 
 var switchMappings = new Dictionary<string, string>()
