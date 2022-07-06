85821a78e6a7091e955713910ed346e4c8cb57ac
Elijah Zulu <modeaxe@gmail.com>
2022-07-05 23:24:30 -0400

    Switched to a logger for warning and information reporting

---
+++ src/Microsoft.PowerApps.TestEngine.Tests/TestStudioConverter/CreateYAMLTestPlanTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.PowerApps.TestEngine.Config;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace Microsoft.PowerApps.TestEngine.Tests.TestStudioConverter
+{
+    class CreateYAMLTestPlanTests
+    {
+        
+    }
+}
+
+
