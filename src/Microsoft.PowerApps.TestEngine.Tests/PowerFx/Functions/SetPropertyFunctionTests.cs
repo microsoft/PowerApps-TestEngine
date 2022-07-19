55d9cd727cc567c6c2fb4aaa3fb8831364b4e126
Landon Rogers <landonrogers@microsoft.com>
2022-07-19 11:35:21 -0400

    SetPropertyFunctionTests, more appropriate names and comments

--- src/Microsoft.PowerApps.TestEngine.Tests/PowerFx/Functions/SetPropertyFunctionTests.cs
+++ src/Microsoft.PowerApps.TestEngine.Tests/PowerFx/Functions/SetPropertyFunctionTests.cs
@@ -75,15 +75,15 @@ namespace Microsoft.PowerApps.TestEngine.Tests.PowerFx.Functions
         {
             MockPowerAppFunctions.Setup(x => x.SetPropertyAsync(It.IsAny<ItemPath>(), It.IsAny<NumberValue>())).Returns(Task.FromResult(true));
 
-            // Make setPropertyFunction contain a text component called Button1
-            var recordType = new RecordType().Add("Text", FormulaType.Number);
+            // Make setPropertyFunction contain a component called Rating1
+            var recordType = new RecordType().Add("Value", FormulaType.Number);
             var recordValue = new ControlRecordValue(recordType, MockPowerAppFunctions.Object, "Rating1");
             var setPropertyFunction = new SetPropertyFunctionNumber(MockPowerAppFunctions.Object);
 
-            // Set the value of Button1's 'Text' property to 5
-            var result = setPropertyFunction.Execute(recordValue, StringValue.New("Default"), NumberValue.New(5));
+            // Set the value of Rating1's 'Value' property to 5
+            var result = setPropertyFunction.Execute(recordValue, StringValue.New("Value"), NumberValue.New(5));
 
-            // check to see if the value of Button1's 'Text' property is 5
+            // check to see if the value of Rating1's 'Value' property is 5
             Assert.IsType<BlankValue>(result);
             MockPowerAppFunctions.Verify(x => x.SetPropertyAsync(It.Is<ItemPath>((item) => item.ControlName == recordValue.Name), It.Is<NumberValue>(numVal => numVal.Value == 5)), Times.Once());
         }
@@ -93,15 +93,15 @@ namespace Microsoft.PowerApps.TestEngine.Tests.PowerFx.Functions
         {
             MockPowerAppFunctions.Setup(x => x.SetPropertyAsync(It.IsAny<ItemPath>(), It.IsAny<BooleanValue>())).Returns(Task.FromResult(true));
 
-            // Make setPropertyFunction contain a text component called Button1
-            var recordType = new RecordType().Add("Text", FormulaType.Boolean);
+            // Make setPropertyFunction contain a component called Toggle1
+            var recordType = new RecordType().Add("Value", FormulaType.Boolean);
             var recordValue = new ControlRecordValue(recordType, MockPowerAppFunctions.Object, "Toggle1");
             var setPropertyFunction = new SetPropertyFunctionBoolean(MockPowerAppFunctions.Object);
 
-            // Set the value of Button1's 'Text' property to 5
-            var result = setPropertyFunction.Execute(recordValue, StringValue.New("Default"), BooleanValue.New(true));
+            // Set the value of Toggle1's 'Value' property to true
+            var result = setPropertyFunction.Execute(recordValue, StringValue.New("Value"), BooleanValue.New(true));
 
-            // check to see if the value of Button1's 'Text' property is 5
+            // check to see if the value of Toggle1's 'Value' property is true
             Assert.IsType<BlankValue>(result);
             MockPowerAppFunctions.Verify(x => x.SetPropertyAsync(It.Is<ItemPath>((item) => item.ControlName == recordValue.Name), It.Is<BooleanValue>(boolVal => boolVal.Value == true)), Times.Once());
         }
@@ -111,16 +111,16 @@ namespace Microsoft.PowerApps.TestEngine.Tests.PowerFx.Functions
         {
             MockPowerAppFunctions.Setup(x => x.SetPropertyAsync(It.IsAny<ItemPath>(), It.IsAny<DateValue>())).Returns(Task.FromResult(true));
 
-            // Make setPropertyFunction contain a text component called Button1
-            var recordType = new RecordType().Add("Text", FormulaType.Date);
+            // Make setPropertyFunction contain a component called DatePicker1
+            var recordType = new RecordType().Add("Value", FormulaType.Date);
             var recordValue = new ControlRecordValue(recordType, MockPowerAppFunctions.Object, "DatePicker1");
             var setPropertyFunction = new SetPropertyFunctionDate(MockPowerAppFunctions.Object);
 
-            // Set the value of Button1's 'Text' property to 5
+            // Set the value of DatePicker1's 'Value' property to the datetime (01/01/2030)
             var dt = new DateTime(2030, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
-            var result = setPropertyFunction.Execute(recordValue, StringValue.New("Default"), FormulaValue.NewDateOnly(dt));
+            var result = setPropertyFunction.Execute(recordValue, StringValue.New("Value"), FormulaValue.NewDateOnly(dt));
 
-            // check to see if the value of Button1's 'Text' property is 5
+            // check to see if the value of DatePicker1's 'Value' property is the correct datetime (01/01/2030)
             Assert.IsType<BlankValue>(result);
             MockPowerAppFunctions.Verify(x => x.SetPropertyAsync(It.Is<ItemPath>((item) => item.ControlName == recordValue.Name), It.Is<DateValue>(dateVal => dateVal.Value == dt)), Times.Once());
         }
