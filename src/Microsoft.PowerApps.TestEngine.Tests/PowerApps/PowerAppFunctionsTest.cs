a3e079a033bfec2008f4dc481d8fb6fb02808ac8
Landon Rogers <landonrogers@microsoft.com>
2022-07-20 16:22:15 -0400

    rm newline

--- src/Microsoft.PowerApps.TestEngine.Tests/PowerApps/PowerAppFunctionsTest.cs
+++ src/Microsoft.PowerApps.TestEngine.Tests/PowerApps/PowerAppFunctionsTest.cs
@@ -36,7 +36,6 @@ namespace Microsoft.PowerApps.TestEngine.Tests.PowerApps
         private Mock<ITestState> MockTestState;
         private Mock<ISingleTestInstanceState> MockSingleTestInstanceState;
         private Mock<ILogger> MockLogger;
-        
 
         public PowerAppFunctionsTest()
         {
