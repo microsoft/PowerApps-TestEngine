fce22c1890f5b84cf053eeb354225baee6e47fad
Landon Rogers <landonrogers@microsoft.com>
2022-07-21 13:08:38 -0400

    Initial

--- src/Microsoft.PowerApps.TestEngine/PowerApps/PowerFxModel/ControlRecordValue.cs
+++ src/Microsoft.PowerApps.TestEngine/PowerApps/PowerFxModel/ControlRecordValue.cs
@@ -116,6 +116,8 @@ namespace Microsoft.PowerApps.TestEngine.PowerApps.PowerFxModel
                         result = BooleanValue.New(bool.Parse(jsPropertyValueModel.PropertyValue));
                         return true;
                     }
+                    /* TODO: When .Date and .DateTime not ambiguous, uncomment
+                     * Currently waiting on PowerFX 'DateTime' and 'Date' types to be less ambiguous, so that both can be used
                     else if (fieldType is DateTimeType)
                     {
                         double milliseconds;
@@ -137,8 +139,7 @@ namespace Microsoft.PowerApps.TestEngine.PowerApps.PowerFxModel
 
                         return true;
                     }
-                    /* TODO: When .Date and .DateTime not ambiguous, uncomment
-                     * Currently waiting on PowerFX 'DateTime' and 'Date' types to be less ambiguous, so that both can be used
+                    */
                     else if (fieldType is DateType)
                     {
                         double milliseconds;
@@ -160,7 +161,6 @@ namespace Microsoft.PowerApps.TestEngine.PowerApps.PowerFxModel
 
                         return true;
                     }
-                    */
 
                     result = New(jsPropertyValueModel.PropertyValue);
                     return true;
