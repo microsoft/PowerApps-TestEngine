51901a2e88ea54b14b05c654831c1b541797b384
Rachel <83382319+Super-Sage@users.noreply.github.com>
2022-08-01 14:28:47 -0400

    update

--- src/Microsoft.PowerApps.TestEngine/JS/PublishedAppTesting.js
+++ src/Microsoft.PowerApps.TestEngine/JS/PublishedAppTesting.js
@@ -180,9 +180,6 @@ function setPropertyValueForControl(itemPath, value) {
 
 function interactWithControl(itemPath, value) {
     var e = AppMagic.AuthoringTool.Runtime.getNamedControl(itemPath.controlName, AppMagic.Controls.GlobalContextManager.bindingContext).OpenAjax.interactWithControlAsync(AppMagic.Controls.GlobalContextManager.bindingContext.controlContexts[itemPath.controlName], value).then(() => true,() => false);
-    console.log(value);
-    console.log(itemPath);
-    console.log("Hey");
-    console.log(typeof itemPath);
+    
     return e._value;
 }
