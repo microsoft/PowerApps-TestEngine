d3f86db26312340c9ae7a0887fb35a1d50523498
Rachel <83382319+Super-Sage@users.noreply.github.com>
2022-08-05 11:00:42 -0400

    Indentation

--- src/Microsoft.PowerApps.TestEngine/TestInfra/PlaywrightTestInfraFunctions.cs
+++ src/Microsoft.PowerApps.TestEngine/TestInfra/PlaywrightTestInfraFunctions.cs
@@ -67,7 +67,7 @@ namespace Microsoft.PowerApps.TestEngine.TestInfra
 
             var launchOptions = new BrowserTypeLaunchOptions()
             {
-                Headless = testSettings.Headless,
+                Headless = false,
                 Timeout = testSettings.Timeout
             };
 
@@ -263,12 +263,12 @@ namespace Microsoft.PowerApps.TestEngine.TestInfra
         {
             ValidatePage();
             var santizedArguments = new string[arguments.Length];
-                       for (int i = 0; i < arguments.Length; i++)
-                       { // encode and add to sanitizedArguments 
-                           var argument = arguments[i];
-                           var encode = Uri.EscapeDataString(argument);
-                           santizedArguments[i] = encode;
-                       }
+            for (int i = 0; i < arguments.Length; i++)
+            { // encode and add to sanitizedArguments 
+                var argument = arguments[i];
+                var encode = Uri.EscapeDataString(argument);
+                santizedArguments[i] = encode;
+            }
             return await Page.EvaluateAsync<T>(jsExpression, santizedArguments);
         }
 
