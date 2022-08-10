a86df395e8ab19e89c78aa028c49dee95f3bdb7f
Landon Rogers <landonrogers@microsoft.com>
2022-08-10 13:06:23 -0400

    add disposable

---
+++ src/Microsoft.PowerApps.TestEngine/Helpers/Disposable.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.PowerApps.TestEngine.Helpers
+{
+    /// <summary>
+    /// Logger to capture logs for test reporting
+    /// </summary>
+    public class Disposable : IDisposable
+    {
+        public Disposable()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
