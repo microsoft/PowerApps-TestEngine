953ece1d0f65d1ea690f2088a9ee03222d37b14e
Navaneeth Thekkumpat <nthekkumpat@microsoft.com>
2022-08-12 18:43:58 -0400

    updating Setproperty function to take just two params

--- src/Microsoft.PowerApps.TestEngine/PowerFx/Functions/SetPropertyFunction.cs
+++ src/Microsoft.PowerApps.TestEngine/PowerFx/Functions/SetPropertyFunction.cs
@@ -21,10 +21,16 @@ namespace Microsoft.PowerApps.TestEngine.PowerFx.Functions
         protected readonly IPowerAppFunctions _powerAppFunctions;
         protected readonly ILogger _logger;
 
-        public SetPropertyFunction(IPowerAppFunctions powerAppFunctions, FormulaType formulaType, ILogger logger) : base("SetProperty", FormulaType.Blank, RecordType.Empty(), FormulaType.String, formulaType)
+        public SetPropertyFunction(IPowerAppFunctions powerAppFunctions, ILogger logger) : base("SetProperty", FormulaType.Blank, RecordType.Empty(), FormulaType.String, FormulaType.Boolean)
         {
             _powerAppFunctions = powerAppFunctions;
             _logger = logger;
+        }
+
+        public BooleanValue Execute(RecordValue obj, StringValue propName, TableValue value)
+        {
+            SetProperty(obj, propName, value).Wait();
+            return FormulaValue.New(true);
         }
 
         protected async Task SetProperty(RecordValue obj, StringValue propName, FormulaValue value)
@@ -50,132 +56,5 @@ namespace Microsoft.PowerApps.TestEngine.PowerFx.Functions
             _logger.LogInformation("Successfully finished executing SetProperty function.");
         }
 
-    }
-
-    public class SetPropertyFunctionNumber : SetPropertyFunction
-    {
-        public SetPropertyFunctionNumber(IPowerAppFunctions powerAppFunctions, ILogger logger) : base(powerAppFunctions, FormulaType.Number, logger)
-        {
-        }
-
-        public BlankValue Execute(RecordValue obj, StringValue propName, NumberValue value)
-        {
-            SetProperty(obj, propName, value).Wait();
-            return FormulaValue.NewBlank();
-        }
-    }
-
-    public class SetPropertyFunctionString : SetPropertyFunction
-    {
-        public SetPropertyFunctionString(IPowerAppFunctions powerAppFunctions, ILogger logger) : base(powerAppFunctions, FormulaType.String, logger)
-        {
-        }
-
-        public BlankValue Execute(RecordValue obj, StringValue propName, StringValue value)
-        {
-            SetProperty(obj, propName, value).Wait();
-            return FormulaValue.NewBlank();
-        }
-    }
-
-    public class SetPropertyFunctionBoolean : SetPropertyFunction
-    {
-        public SetPropertyFunctionBoolean(IPowerAppFunctions powerAppFunctions, ILogger logger) : base(powerAppFunctions, FormulaType.Boolean, logger)
-        {
-        }
-
-        public BlankValue Execute(RecordValue obj, StringValue propName, BooleanValue value)
-        {
-            SetProperty(obj, propName, value).Wait();
-            return FormulaValue.NewBlank();
-        }
-    }
-
-    public class SetPropertyFunctionDate : SetPropertyFunction
-    {
-        public SetPropertyFunctionDate(IPowerAppFunctions powerAppFunctions, ILogger logger) : base(powerAppFunctions, FormulaType.Date, logger)
-        {
-        }
-
-        public BlankValue Execute(RecordValue obj, StringValue propName, DateValue value)
-        {
-            SetProperty(obj, propName, value).Wait();
-            return FormulaValue.NewBlank();
-        }
-    }
-
-    public class SetPropertyFunctionRecord : SetPropertyFunction
-    {
-        public SetPropertyFunctionRecord(IPowerAppFunctions powerAppFunctions, ILogger logger) : base(powerAppFunctions, RecordType.Empty(), logger)
-        {
-        }
-
-        public BlankValue Execute(RecordValue obj, StringValue propName, RecordValue value)
-        {
-            SetProperty(obj, propName, value).Wait();
-            return FormulaValue.NewBlank();
-        }
-    }
-
-    public class SetPropertyFunctionTable : SetPropertyFunction
-    {
-        public SetPropertyFunctionTable(IPowerAppFunctions powerAppFunctions, ILogger logger) : base(powerAppFunctions, TableType.Empty(), logger)
-        {
-        }
-
-        public BlankValue Execute(RecordValue obj, StringValue propName, TableValue value)
-        {
-            SetProperty(obj, propName, value).Wait();
-            return FormulaValue.NewBlank();
-        }
-
-        private async Task SetProperty(RecordValue obj, StringValue propName, TableValue value)
-        {
-            _logger.LogInformation("------------------------------\n\n" +
-                "Executing SetProperty function.");
-
-            NullCheckHelper.NullCheck(obj, propName, value, _logger);
-
-            var controlName = obj.GetType().GetProperty("Name")?.GetValue(obj, null)?.ToString();
-
-            var itemPath = new ItemPath()
-            {
-                ControlName = controlName,
-                Index = null,
-                ParentControl = null,
-                PropertyName = (string)propName.Value
-            };
-
-            var recordType = RecordType.Empty().Add(controlName, RecordType.Empty());
-
-            var controlTableSource = new ControlTableSource(_powerAppFunctions, itemPath, recordType, _logger);
-
-            var powerAppControlModel = new ControlTableValue(recordType, controlTableSource, _powerAppFunctions, _logger);
-            var result = await _powerAppFunctions.SetPropertyAsync(itemPath, value);
-
-            if (!result)
-            {
-                _logger.LogDebug("Error occurred on DataType of type " + value.GetType());
-                _logger.LogTrace("Property name: " + propName);
-                _logger.LogTrace("Property attempted being set to: " + value);
-                _logger.LogTrace(powerAppControlModel.ToString());
-                throw new Exception();
-            }
-
-            _logger.LogInformation("Successfully finished executing SetProperty function.");
-        }
-    }
-
-    public static class SetPropertyRegisterExtensions
-    {
-        public static void RegisterAll(this PowerFxConfig powerFxConfig, IPowerAppFunctions powerAppFunctions, ILogger logger)
-        {
-            powerFxConfig.AddFunction(new SetPropertyFunctionNumber(powerAppFunctions, logger));
-            powerFxConfig.AddFunction(new SetPropertyFunctionString(powerAppFunctions, logger));
-            powerFxConfig.AddFunction(new SetPropertyFunctionBoolean(powerAppFunctions, logger));
-            powerFxConfig.AddFunction(new SetPropertyFunctionDate(powerAppFunctions, logger));
-            powerFxConfig.AddFunction(new SetPropertyFunctionRecord(powerAppFunctions, logger));
-            powerFxConfig.AddFunction(new SetPropertyFunctionTable(powerAppFunctions, logger));
-        }
     }
 }
