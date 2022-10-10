5db8c4bfcccb32f77a57ad2e3ce0f0a113f028f1
Navaneeth Thekkumpat <98551644+NavneetThekkumpat@users.noreply.github.com>
2022-10-10 16:17:56 -0400

    Handling browser not found exception (#154)
    * handling browser not supported cases
    
    * unittesting
    
    * refactoring
    
    * udpating doc
    
    * updating doc
    
    * updating doc
    
    * refactoring
    
    * updating exception message and unit test

--- src/Microsoft.PowerApps.TestEngine/TestInfra/PlaywrightTestInfraFunctions.cs
+++ src/Microsoft.PowerApps.TestEngine/TestInfra/PlaywrightTestInfraFunctions.cs
@@ -75,8 +75,14 @@ namespace Microsoft.PowerApps.TestEngine.TestInfra
                 Timeout = testSettings.Timeout
             };
 
+            var browser = PlaywrightObject[browserConfig.Browser];
+            if (browser == null)
+            {
+                _singleTestInstanceState.GetLogger().LogError("Browser not supported by Playwright, for more details check https://playwright.dev/dotnet/docs/browsers");
+                throw new InvalidOperationException("Browser not supported.");
+            }
 
-            Browser = await PlaywrightObject[browserConfig.Browser].LaunchAsync(launchOptions);
+            Browser = await browser.LaunchAsync(launchOptions);
 
             var contextOptions = new BrowserNewContextOptions();
 
