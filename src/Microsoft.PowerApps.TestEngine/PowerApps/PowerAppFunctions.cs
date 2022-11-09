955df1aceb25edae287b8f190db67ad7614c3d56
clairewu0218 <104450101+clairewu0218@users.noreply.github.com>
2022-11-09 13:27:45 -0500

    Stringify return object for getPropertyValue (#172)
    * Stringify return object from getPropertyValue

--- src/Microsoft.PowerApps.TestEngine/PowerApps/PowerAppFunctions.cs
+++ src/Microsoft.PowerApps.TestEngine/PowerApps/PowerAppFunctions.cs
@@ -20,7 +20,6 @@ namespace Microsoft.PowerApps.TestEngine.PowerApps
         private readonly ISingleTestInstanceState _singleTestInstanceState;
         private readonly ITestState _testState;
         private bool IsPlayerJsLoaded { get; set; } = false;
-        private bool IsPublishedAppTestingJsLoaded { get; set; } = false;
         private string PublishedAppIframeName { get; set; } = "fullscreen-app-host";
         private string GetAppStatusErrorMessage = "Something went wrong when Test Engine try to get App status.";
         private string GetItemCountErrorMessage = "Something went wrong when Test Engine try to get item count.";
@@ -39,7 +38,7 @@ namespace Microsoft.PowerApps.TestEngine.PowerApps
         {
             ValidateItemPath(itemPath, true);
             var itemPathString = JsonConvert.SerializeObject(itemPath);
-            var expression = $"PowerAppsTestEngine.getPropertyValue({itemPathString})";
+            var expression = $"PowerAppsTestEngine.getPropertyValue({itemPathString}).then((propertyValue) => JSON.stringify(propertyValue));";
             return await _testInfraFunctions.RunJavascriptAsync<T>(expression);
         }
 
@@ -68,21 +67,12 @@ namespace Microsoft.PowerApps.TestEngine.PowerApps
         {
             try
             {
-                // This is for client side JSSDK
-                // if (!IsPlayerJsLoaded)
-                // {
-                //     await _testInfraFunctions.AddScriptTagAsync(GetFilePath(Path.Combine("JS", "CanvasAppSdk.js")), null);
-                //     IsPlayerJsLoaded = true;
-                // }
-
-                // TODO: update the expression when the server side JSSDK is released
                 var expression = "PowerAppsTestEngine.getAppStatus()";
                 return (await _testInfraFunctions.RunJavascriptAsync<string>(expression)) == "Idle";
             }
             catch (Exception ex)
             {
                 _singleTestInstanceState.GetLogger().LogDebug(ex.ToString());
-                IsPlayerJsLoaded = false;
                 return false;
             }
 
@@ -147,13 +137,6 @@ namespace Microsoft.PowerApps.TestEngine.PowerApps
         public async Task<Dictionary<string, ControlRecordValue>> LoadPowerAppsObjectModelAsync()
         {
             await PollingHelper.PollAsync<bool>(false, (x) => !x, () => CheckIfAppIsIdleAsync(), _testState.GetTestSettings().Timeout, _singleTestInstanceState.GetLogger(), GetAppStatusErrorMessage);
-
-            // This is for client side JSSDK
-            // if (!IsPublishedAppTestingJsLoaded)
-            // {
-            //     await _testInfraFunctions.AddScriptTagAsync(GetFilePath(Path.Combine("JS", "PublishedAppTesting.js")), PublishedAppIframeName);
-            //     IsPublishedAppTestingJsLoaded = true;
-            // }
 
             var controlDictionary = new Dictionary<string, ControlRecordValue>();
             _singleTestInstanceState.GetLogger().LogDebug("Start to load power apps object model");
