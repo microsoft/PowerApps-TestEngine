d36ee85b318a5c72b9622b8db82ec0f207d56483
Navaneeth Thekkumpat <nthekkumpat@microsoft.com>
2023-01-11 17:56:27 -0500

    new helper for exception handling

---
+++ src/Microsoft.PowerApps.TestEngine/Helpers/ExceptionHandlingHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.PowerApps.TestEngine.PowerApps;
+
+namespace Microsoft.PowerApps.TestEngine.Helpers
+{
+    internal static class ExceptionHandlingHelper
+    {
+        public static void CheckIfOutDatedPublishedApp(Exception ex, ILogger logger)
+        {
+            if (ex.Message?.ToString() == PowerAppFunctions.PublishedAppWithoutJSSDKErrorCode || ex.InnerException?.Message?.ToString() == PowerAppFunctions.PublishedAppWithoutJSSDKErrorCode)
+            {
+                logger.LogError(PowerAppFunctions.PublishedAppWithoutJSSDKMessage);
+            }
+        }
+    }
+}
