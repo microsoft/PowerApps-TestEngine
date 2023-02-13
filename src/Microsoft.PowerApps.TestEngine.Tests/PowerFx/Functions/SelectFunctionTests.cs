5f6ccd48149965faad15f23eba283c572e718ae3
Landon Rogers <landonrogers@microsoft.com>
2023-02-13 10:38:07 -0500

    adtl logging

--- src/Microsoft.PowerApps.TestEngine.Tests/PowerFx/Functions/SelectFunctionTests.cs
+++ src/Microsoft.PowerApps.TestEngine.Tests/PowerFx/Functions/SelectFunctionTests.cs
@@ -191,7 +191,7 @@ namespace Microsoft.PowerApps.TestEngine.Tests.PowerFx.Functions
             Assert.ThrowsAny<Exception>(() => selectFunction.Execute(recordValue, rowOrColumn));
             MockPowerAppFunctions.Verify(x => x.SelectControlAsync(It.Is<ItemPath>((item) => item.ControlName == recordValue.Name)), Times.Once());
             LoggingTestHelper.VerifyLogging(MockLogger, "------------------------------\n\n" + "Executing Select function.", LogLevel.Information, Times.AtLeastOnce());
-                        LoggingTestHelper.VerifyLogging(MockLogger, "Control name: Gallery1", LogLevel.Trace, Times.AtLeastOnce());
+            LoggingTestHelper.VerifyLogging(MockLogger, "Control name: Gallery1", LogLevel.Trace, Times.AtLeastOnce());
             LoggingTestHelper.VerifyLogging(MockLogger, "Unable to select control", LogLevel.Error, Times.AtLeastOnce());
             Assert.Equal(0, updaterFunctionCallCount);
         }
@@ -231,6 +231,8 @@ namespace Microsoft.PowerApps.TestEngine.Tests.PowerFx.Functions
             Assert.ThrowsAny<Exception>(() => selectFunction.Execute(parentValue, rowOrColumn, childValue));
             MockPowerAppFunctions.Verify(x => x.SelectControlAsync(It.Is<ItemPath>((item) => item.ControlName == childValue.Name)), Times.Once());
             LoggingTestHelper.VerifyLogging(MockLogger, "------------------------------\n\n" + "Executing Select function.", LogLevel.Information, Times.AtLeastOnce());
+            LoggingTestHelper.VerifyLogging(MockLogger, "Control name: Gallery1", LogLevel.Trace, Times.AtLeastOnce());
+            LoggingTestHelper.VerifyLogging(MockLogger, "Unable to select control", LogLevel.Error, Times.AtLeastOnce());
             Assert.Equal(0, updaterFunctionCallCount);
         }
 
