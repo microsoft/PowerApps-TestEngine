b3e44dacf172643d48df347dd58cbb7875b01dd6
Landon Rogers <landonrogers@microsoft.com>
2023-03-28 10:58:44 -0400

    fix err

--- src/Microsoft.PowerApps.TestEngine.Tests/SingleTestRunnerTests.cs
+++ src/Microsoft.PowerApps.TestEngine.Tests/SingleTestRunnerTests.cs
@@ -271,7 +271,7 @@ namespace Microsoft.PowerApps.TestEngine.Tests
 
             SetupMocks(testData.testRunId, testData.testSuiteId, testData.testId, testData.appUrl, testData.testSuiteDefinition, true, testData.additionalFiles, testData.testSuiteLocale);
 
-            var exceptionToThrow = new InvalidOperationException("Test exception");
+            var exceptionToThrow = new Exception("Encountered an error. See the debug log for this test suite for more information.");
             additionalMockSetup(exceptionToThrow);
 
             var locale = string.IsNullOrEmpty(testData.testSuiteLocale) ? CultureInfo.CurrentCulture : new CultureInfo(testData.testSuiteLocale);
