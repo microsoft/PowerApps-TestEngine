1a59929ec89350b046b29f6c42eeb5cf609a2ac9
Landon Rogers <landonrogers@microsoft.com>
2023-04-27 15:58:42 -0400

    fix consoleout

--- src/Microsoft.PowerApps.TestEngine/Helpers/LoggingHelper.cs
+++ src/Microsoft.PowerApps.TestEngine/Helpers/LoggingHelper.cs
@@ -43,7 +43,7 @@ namespace Microsoft.PowerApps.TestEngine.Helpers
             {
                 Logger.LogDebug("Issue getting DebugInfo. This can be a result of not being properly logged in.");
                 Logger.LogDebug(ex.ToString());
-                Console.Out.WriteLine("[Critical Error]: Could not access PowerApps. Confirm you are logged in properly.");
+                Console.WriteLine("[Critical Error]: Could not access PowerApps. Confirm you are logged in properly.");
             }
         }
     }
