656f9a610e6c6a7649647944ceb2eb5b447dc9bb
Landon Rogers <landonrogers@microsoft.com>
2023-04-29 12:39:26 -0400

    fix err

--- src/Microsoft.PowerApps.TestEngine/TestEngineEvents.cs
+++ src/Microsoft.PowerApps.TestEngine/TestEngineEvents.cs
@@ -13,37 +13,37 @@ namespace Microsoft.PowerApps.TestEngine
     {
         public void AssertionFailed(string assertion)
         {
-            Console.Write("Assertion failed: " + assertion);
+
         }
 
         public void EncounteredException(Exception ex)
         {
-            Console.Write("Encountered exception: " + ex);
+
         }
 
         public void SuiteBegin(string suiteName, string directory, string browserName, string url)
         {
-            Console.Write("Suite began: " + suiteName);
+
         }
 
         public void SuiteEnd()
         {
-            Console.Write("Suite ended");
+
         }
 
         public void TestCaseBegin(string name)
         {
-            Console.Write("Test case began: " + name);
+
         }
 
         public void TestCaseEnd(bool result)
         {
-            Console.Write("Test case ended. Passed: " + result);
+
         }
 
         public void TestReportPath(string path)
         {
-            Console.Write("Test report path: " + path);
+            
         }
     }
 }
