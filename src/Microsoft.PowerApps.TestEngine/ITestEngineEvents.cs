9125a10570c4e0f3218c34308f60f23654f8e59e
Landon Rogers <landonrogers@microsoft.com>
2023-04-29 11:33:29 -0400

    add addtl file

---
+++ src/Microsoft.PowerApps.TestEngine/ITestEngineEvents.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Logging;
+using Microsoft.PowerApps.TestEngine.Config;
+
+namespace Microsoft.PowerApps.TestEngine
+{
+    /// <summary>
+    /// Triggers for every major Test Engine event
+    /// </summary>
+    public interface ITestEngineEvents
+    {
+        /// <summary>
+        /// Handles the assertion message when the Assert() function fails
+        /// </summary>
+        public void AssertionFailed(string assertion);
+
+        /// <summary>
+        /// Handles logging for an exception
+        /// </summary>
+        public void EncounteredException(Exception ex);
+
+        /// <summary>
+        /// Handles the starting suite output
+        /// </summary>
+        public void SuiteBegin(string suiteName, string directory, string browserName, string url);
+
+        /// <summary>
+        /// Handles the suite summary output
+        /// </summary>
+        public void SuiteEnd();
+
+        /// <summary>
+        /// Handles the starting case output
+        /// </summary>
+        public void TestCaseBegin(string name);
+
+        /// <summary>
+        /// Handles result of a test case
+        /// </summary>
+        public void TestCaseEnd(bool result);
+
+        /// <summary>
+        /// Handles the output for test report path
+        /// </summary>
+        public void TestReportPath(string path);
+    }
+}
---
+++ src/Microsoft.PowerApps.TestEngine/TestEngineEvents.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Logging;
+using Microsoft.PowerApps.TestEngine.Config;
+
+namespace Microsoft.PowerApps.TestEngine
+{
+    /// <summary>
+    /// Handles all major Test Engine events using console output
+    /// </summary>
+    public class TestEngineEvents : ITestEngineEvents
+    {
+        public void AssertionFailed(string assertion)
+        {
+            Console.Write("Assertion failed: " + assertion);
+        };
+
+        public void EncounteredException(Exception ex)
+        {
+            Console.Write("Encountered exception: " + ex);
+        }
+
+        public void SuiteBegin(string suiteName, string directory, string browserName, string url)
+        {
+            Console.Write("Suite began: " + suiteName);
+        }
+
+        public void SuiteEnd()
+        {
+            Console.Write("Suite ended");
+        }
+
+        public void TestCaseBegin(string name)
+        {
+            Console.Write("Test case began: " + name);
+        }
+
+        public void TestCaseEnd(bool result)
+        {
+            Console.Write("Test case ended. Passed: " + result);
+        }
+
+        public void TestReportPath(string path)
+        {
+            Console.Write("Test report path: " + path);
+        }
+    }
+}
