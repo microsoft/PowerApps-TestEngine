4de17787551d5ec92ec88c70e8a5de25f935f5e8
Landon Rogers <landonrogers@microsoft.com>
2023-05-03 18:50:43 -0400

    Initial

---
+++ src/Microsoft.PowerApps.TestEngine/System/AssertionFailureException.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+
+namespace Microsoft.PowerApps.TestEngine.System
+{
+    public class AssertionFailureException : Exception
+    {
+        public AssertionFailureException(string message)
+            : base(message)
+        {
+        }
+    }
+}
