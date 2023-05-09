ff4f8fd2a5a39bac77a47fa280c8baa10283a0d0
Landon Rogers <landonrogers@microsoft.com>
2023-05-09 07:40:20 -0400

    fix err

--- src/Microsoft.PowerApps.TestEngine.Tests/ConsoleOutputTests.cs
+++ src/Microsoft.PowerApps.TestEngine.Tests/ConsoleOutputTests.cs
@@ -148,7 +148,6 @@ namespace Microsoft.PowerApps.TestEngine.Tests
             // Assert that the expected output matches the console output of the function
             Assert.AreEqual(printer.ToString(), expected);
         }
-
         [Fact]
         public void TestTestCaseEndFailed()
         {
