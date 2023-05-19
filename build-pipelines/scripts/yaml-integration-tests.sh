6f8de97597b3662b1d04371144a438456f84e55d
Landon Rogers <100798954+landonarogers@users.noreply.github.com>
2023-05-19 11:19:04 -0400

    Integration Testing for All Players (#213)

--- build-pipelines/scripts/yaml-integration-tests.sh
+++ build-pipelines/scripts/yaml-integration-tests.sh
@@ -44,6 +44,7 @@ do
        fi                                                                
     done
   if [[ -n "${envId}" && -n "${tenantId}" && -n "${domain}" && -n "${testPlanFile}" && -n "${outputDir}" ]]; then     # null checks on args
-    dotnet run -f net6.0 -- -e ${envId} -t ${tenantId} -d ${domain} -i ${testPlanFile} -o ${outputDir};  
+    dotnet run -f net6.0 -- -e ${envId} -t ${tenantId} -d ${domain} -i ${testPlanFile} -o ${outputDir} -q "&PAOverrideFGRollout.OnePlayerStandaloneWebPlayer=false";
+    dotnet run -f net6.0 -- -e ${envId} -t ${tenantId} -d ${domain} -i ${testPlanFile} -o ${outputDir} -q "&PAOverrideFGRollout.OnePlayerStandaloneWebPlayer=true";
   fi
 done
