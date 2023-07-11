3bb160a191b320793d7cc248dc90f37d33ccff4c
Landon Rogers <landonrogers@microsoft.com>
2023-07-11 12:53:17 -0400

    spacing

--- src/Microsoft.PowerApps.TestEngine.Tests/SingleTestRunnerTests.cs
+++ src/Microsoft.PowerApps.TestEngine.Tests/SingleTestRunnerTests.cs
@@ -225,7 +225,7 @@ namespace Microsoft.PowerApps.TestEngine.Tests
             SetupMocks(testData.testRunId, testData.testSuiteId, testData.testId, testData.appUrl, testData.testSuiteDefinition, true, additionalFiles, testData.testSuiteLocale);
 
             var locale = string.IsNullOrEmpty(testData.testSuiteLocale) ? CultureInfo.CurrentCulture : new CultureInfo(testData.testSuiteLocale);
-        
+
             await singleTestRunner.RunTestAsync(testData.testRunId, testData.testRunDirectory, testData.testSuiteDefinition, testData.browserConfig, "", "", locale);
 
             VerifyTestStateSetup(testData.testSuiteId, testData.testRunId, testData.testSuiteDefinition, testData.testResultDirectory, testData.browserConfig);
@@ -306,7 +306,7 @@ namespace Microsoft.PowerApps.TestEngine.Tests
             additionalMockSetup(exceptionToThrow);
 
             var locale = string.IsNullOrEmpty(testData.testSuiteLocale) ? CultureInfo.CurrentCulture : new CultureInfo(testData.testSuiteLocale);
-
+            
             await singleTestRunner.RunTestAsync(testData.testRunId, testData.testRunDirectory, testData.testSuiteDefinition, testData.browserConfig, "", "", locale);
 
             VerifyTestStateSetup(testData.testSuiteId, testData.testRunId, testData.testSuiteDefinition, testData.testResultDirectory, testData.browserConfig);
