d0d29da140eaca395087065256e2cd58939958b6
Navaneeth Thekkumpat <nthekkumpat@microsoft.com>
2023-08-01 14:01:24 -0400

    updating tests

--- src/Microsoft.PowerApps.TestEngine.Tests/PowerApps/PowerAppFunctionsTest.cs
+++ src/Microsoft.PowerApps.TestEngine.Tests/PowerApps/PowerAppFunctionsTest.cs
@@ -600,7 +600,10 @@ namespace Microsoft.PowerApps.TestEngine.Tests.PowerApps
             MockSingleTestInstanceState.Setup(x => x.GetLogger()).Returns(MockLogger.Object);
 
             var newSession = new ExpandoObject();
-            newSession.TryAdd("sessionID", "somesessionId");
+            newSession.TryAdd("appId", "someAppId");
+            newSession.TryAdd("appVersion", "someAppVersionId");
+            newSession.TryAdd("environmentId", "someEnvironmentId");
+            newSession.TryAdd("sessionId", "someSessionId");
 
             MockTestInfraFunctions.Setup(x => x.AddScriptTagAsync(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
             MockTestInfraFunctions.SetupSequence(x => x.RunJavascriptAsync<object>("PowerAppsTestEngine.debugInfo"))
