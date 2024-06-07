5c010d1613788d0bab628ac641c78743bedffbb7
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2024-06-06 20:14:20 -0700

    Implement IUserManager for #339

---
+++ src/testengine.user.local.tests/LocalUserManagerModuleTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
+using Microsoft.PowerApps.TestEngine.Config;
+using Microsoft.PowerApps.TestEngine.System;
+using Microsoft.PowerApps.TestEngine.TestInfra;
+using Microsoft.PowerFx;
+using Moq;
+
+namespace testengine.user.local.tests
+{
+    public class LocalUserManagerModuleTests
+    {
+        private Mock<IBrowserContext> MockBrowserState;
+        private Mock<ITestInfraFunctions> MockTestInfraFunctions;
+        private Mock<ITestState> MockTestState;
+        private Mock<ISingleTestInstanceState> MockSingleTestInstanceState;
+        private Mock<IEnvironmentVariable> MockEnvironmentVariable;
+        private Mock<ILogger> MockLogger;
+        private Mock<IBrowserContext> MockBrowserContext;
+        private Mock<IPage> MockPage;
+        private Mock<IElementHandle> MockElementHandle;
+        private Mock<IFileSystem> MockFileSystem;
+
+        public LocalUserManagerModuleTests()
+        {
+            MockBrowserState = new Mock<IBrowserContext>(MockBehavior.Strict);
+            MockTestInfraFunctions = new Mock<ITestInfraFunctions>(MockBehavior.Strict);
+            MockTestState = new Mock<ITestState>(MockBehavior.Strict);
+            MockSingleTestInstanceState = new Mock<ISingleTestInstanceState>(MockBehavior.Strict);
+            MockEnvironmentVariable = new Mock<IEnvironmentVariable>(MockBehavior.Strict);
+            MockLogger = new Mock<ILogger>(MockBehavior.Strict);
+            MockBrowserContext = new Mock<IBrowserContext>(MockBehavior.Strict);
+            MockPage = new Mock<IPage>(MockBehavior.Strict);
+            MockElementHandle = new Mock<IElementHandle>(MockBehavior.Strict);
+            MockFileSystem = new Mock<IFileSystem>(MockBehavior.Strict);
+        }
+
+        [Fact]
+        public void NameIsLocal()
+        {
+            var userManager = new LocalUserManagerModule();
+
+            Assert.Equal("local", userManager.Name);
+        }
+
+        [Fact]
+        public void NotUseStaticContext()
+        {
+            var userManager = new LocalUserManagerModule();
+
+            Assert.False(userManager.UseStaticContext);
+        }
+
+        [Fact]
+        public async Task LoginWithBrowserState()
+        {
+            // Arrange
+            var userManager = new LocalUserManagerModule();
+
+            // Act
+            await userManager.LoginAsUserAsync("*",
+                MockBrowserState.Object,
+                MockTestState.Object,
+                MockSingleTestInstanceState.Object,
+                MockEnvironmentVariable.Object);
+
+            // Assert
+        }
+    }
+}
---
+++ src/testengine.user.local/LocalUserManagerModule.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.PowerFx;
+using System.ComponentModel.Composition;
+using Microsoft.Extensions.Logging;
+using Microsoft.PowerApps.TestEngine.Config;
+using Microsoft.PowerApps.TestEngine.Modules;
+using Microsoft.PowerApps.TestEngine.System;
+using Microsoft.PowerApps.TestEngine.TestInfra;
+using Microsoft.Playwright;
+using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using Microsoft.PowerApps.TestEngine.Users;
+
+namespace testengine.user.local
+{
+    [Export(typeof(IUserManager))]
+    public class LocalUserManagerModule : IUserManager
+    {
+        public string Name { get { return "local"; } }
+
+        public int Priority { get { return 50; } }
+
+        public bool UseStaticContext { get { return false; } }
+
+        public string Location { get; set; } = "";
+
+        private IBrowserContext? Context { get; set; }
+
+        public IPage? Page { get; set; }
+
+        public async Task LoginAsUserAsync(
+            string desiredUrl,
+            IBrowserContext context,
+            ITestState testState,
+            ISingleTestInstanceState singleTestInstanceState,
+            IEnvironmentVariable environmentVariable)
+        {
+            await Task.CompletedTask;
+        }
+
+    }
+}
