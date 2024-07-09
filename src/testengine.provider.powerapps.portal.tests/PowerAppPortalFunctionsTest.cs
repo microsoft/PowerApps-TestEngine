eaab6d74225c6d6d8b06a6995982e640df0c6fd3
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2024-07-09 14:58:04 -0700

    Initial portal provider

---
+++ src/testengine.provider.powerapps.portal.tests/PowerAppPortalFunctionsTest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.PowerApps.TestEngine.Config;
+using Microsoft.PowerApps.TestEngine.Helpers;
+using Microsoft.PowerApps.TestEngine.Providers;
+using Microsoft.PowerApps.TestEngine.TestInfra;
+using Microsoft.PowerApps.TestEngine.Tests.Helpers;
+using Microsoft.PowerFx.Types;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Microsoft.PowerApps.TestEngine.Tests.PowerApps
+{
+    public class PowerAppPortalFunctionsTest
+    {
+        private Mock<ITestInfraFunctions> MockTestInfraFunctions;
+        private Mock<ITestState> MockTestState;
+        private Mock<ISingleTestInstanceState> MockSingleTestInstanceState;
+        private Mock<ILogger> MockLogger;
+
+        public PowerAppPortalFunctionsTest()
+        {
+            MockTestInfraFunctions = new Mock<ITestInfraFunctions>(MockBehavior.Strict);
+            MockTestState = new Mock<ITestState>(MockBehavior.Strict);
+            MockSingleTestInstanceState = new Mock<ISingleTestInstanceState>(MockBehavior.Strict);
+            MockLogger = new Mock<ILogger>(MockBehavior.Strict);
+        }
+
+
+        [Fact]
+        public void ExpectedName()
+        {
+            // Arrange
+            var provider = new PowerAppPortalFunctions();
+
+            // Act
+            var name = provider.Name;
+
+            // Assert
+            Assert.Equal("powerapps.portal", name);
+        }
+    }
+}
