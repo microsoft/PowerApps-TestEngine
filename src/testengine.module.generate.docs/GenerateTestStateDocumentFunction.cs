cdaa0d97d2fac6fe945cef8c4d9601d8437846a7
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2024-07-22 18:35:23 -0700

    Adding doc generation

---
+++ src/testengine.module.generate.docs/GenerateTestStateDocumentFunction.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
+using Microsoft.PowerApps.TestEngine.Config;
+using Microsoft.PowerApps.TestEngine.TestInfra;
+using Microsoft.PowerFx;
+using Microsoft.PowerFx.Types;
+
+namespace testengine.module
+{
+    /// <summary>
+    /// This will pause the current test and allow the user to interact with the browser and inspect state when headless mode is false
+    /// </summary>
+    public class GenerateTestStateDocumentFunction : ReflectionFunction
+    {
+        private readonly ITestInfraFunctions _testInfraFunctions;
+        private readonly ITestState _testState;
+        private readonly ILogger _logger;
+        private readonly PowerFxConfig _config;
+
+        public GenerateTestStateDocumentFunction(ITestInfraFunctions testInfraFunctions, ITestState testState, ILogger logger, PowerFxConfig config)
+            : base("GenerateTestStateDocument", FormulaType.Blank, StringType.String)
+        {
+            _testInfraFunctions = testInfraFunctions;
+            _testState = testState;
+            _logger = logger;
+            _config = config;
+        }
+
+        public BlankValue Execute(StringValue file)
+        {
+            using ( var output = new StreamWriter(file.Value) )
+            {
+                output.WriteLine("Functions");
+                foreach (var function in _config.SymbolTable.FunctionNames)
+                {
+                    output.WriteLine(function);
+                }
+
+                output.WriteLine("Variables");
+                foreach ( var symbol in _config.SymbolTable.SymbolNames)
+                {
+                    output.WriteLine(symbol.DisplayName.Value);
+                }
+                output.WriteLine();
+            }
+            return FormulaValue.NewBlank();
+        }
+    }
+}
+
---
+++ src/testengine.module.generate.docs/GenerateTestStateDocumentModule.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.ComponentModel.Composition;
+using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
+using Microsoft.PowerApps.TestEngine.Config;
+using Microsoft.PowerApps.TestEngine.Modules;
+using Microsoft.PowerApps.TestEngine.Providers;
+using Microsoft.PowerApps.TestEngine.System;
+using Microsoft.PowerApps.TestEngine.TestInfra;
+using Microsoft.PowerFx;
+
+namespace testengine.module
+{
+    [Export(typeof(ITestEngineModule))]
+    public class GenerateTestStateDocumentModule : ITestEngineModule
+    {
+        public void ExtendBrowserContextOptions(BrowserNewContextOptions options, TestSettings settings)
+        {
+
+        }
+
+        public void RegisterPowerFxFunction(PowerFxConfig config, ITestInfraFunctions testInfraFunctions, ITestWebProvider testWebProvider, ISingleTestInstanceState singleTestInstanceState, ITestState testState, IFileSystem fileSystem)
+        {
+            ILogger logger = singleTestInstanceState.GetLogger();
+            config.AddFunction(new GenerateTestStateDocumentFunction(testInfraFunctions, testState, logger, config));
+            logger.LogInformation("Registered GenerateTestStateDocumentFunction()");
+        }
+
+        public async Task RegisterNetworkRoute(ITestState state, ISingleTestInstanceState singleTestInstanceState, IFileSystem fileSystem, IPage Page, NetworkRequestMock mock)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
