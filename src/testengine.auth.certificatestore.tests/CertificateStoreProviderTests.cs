bab20f2ece847a8d09ea093c56138355156fd106
Sourabh Namilikonda <snamilikonda@microsoft.com>
2024-07-31 14:15:24 -0700

    certificate store retrieval

---
+++ src/testengine.auth.certificatestore.tests/CertificateStoreProviderTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Moq;
+using Xunit;
+
+namespace testengine.auth.certificatestore.tests
+{
+    public class CertificateStoreProviderTests
+    {
+        private readonly CertificateStoreProvider provider;
+
+        public CertificateStoreProviderTests()
+        {
+            provider = new CertificateStoreProvider();
+        }
+
+        [Fact]
+        public void NameProperty_ShouldReturnLocalCert()
+        {
+            // Act
+            var name = provider.Name;
+
+            // Assert
+            Assert.Equal("certstore", name);
+        }
+
+        [Fact]
+        public void RetrieveCertificateForUser_BySubjectName_ReturnsCertificate()
+        {
+            // Arrange
+            string username = Guid.NewGuid().ToString();
+            X509Certificate2 mockCertificate;
+            using (var rsa = RSA.Create(2048))
+            {
+                var request = new CertificateRequest($"CN={username}", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                mockCertificate = request.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1));
+            }
+            try
+            {
+                using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+                {
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Add(mockCertificate);
+                    store.Close();
+                }
+
+                // Act
+                X509Certificate2 certificate = provider.RetrieveCertificateForUser(username);
+
+                // Assert
+                Assert.NotNull(certificate);
+                Assert.Equal(mockCertificate, certificate);
+
+            }
+            finally 
+            {
+                using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+                {
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Remove(mockCertificate);
+                    store.Close();
+                }
+            }
+        }
+
+        [Fact]
+        public void RetrieveCertificateForUser_BySan_ReturnsCertificate()
+        {
+            string username = Guid.NewGuid().ToString();
+            X509Certificate2 mockCertificate;
+            using (var rsa = RSA.Create(2048))
+            {
+                var sanBuilder = new SubjectAlternativeNameBuilder();
+                sanBuilder.AddDnsName(username);
+                var sanExtension = sanBuilder.Build();
+                var request = new CertificateRequest($"CN=extra-{username}", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                request.CertificateExtensions.Add(sanExtension);
+                mockCertificate = request.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(1));
+            }
+            try
+            {
+                using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+                {
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Add(mockCertificate);
+                    store.Close();
+                }
+
+                // Act
+                X509Certificate2 certificate = provider.RetrieveCertificateForUser(username);
+
+                // Assert
+                Assert.NotNull(certificate);
+                Assert.Equal(mockCertificate, certificate);
+
+            }
+            finally
+            {
+                using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+                {
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Remove(mockCertificate);
+                    store.Close();
+                }
+            }
+        }
+
+        [Fact]
+        public void RetrieveCertificateForUser_CertificateNotFound_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            string username = "nonexistentuser";
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => provider.RetrieveCertificateForUser(username));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void RetrieveCertificateForUser_NullOrEmptyUsername_ThrowsArgumentException(string username)
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => provider.RetrieveCertificateForUser(username));
+        }
+    }
+}
---
+++ src/testengine.auth.certificatestore/CertificateStoreProvider.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.ComponentModel.Composition;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.PowerApps.TestEngine.Config;
+
+namespace testengine.auth
+{
+    /// <summary>
+    /// Functions for interacting with the Power App
+    /// </summary>
+    [Export(typeof(IUserCertificateProvider))]
+    public class CertificateStoreProvider : IUserCertificateProvider
+    {
+        internal static Func<X509Store> GetCertStore = () => new X509Store(StoreName.My, StoreLocation.CurrentUser);
+
+        public string Name { get { return "certstore"; } }
+
+        public CertificateStoreProvider()
+        {   
+        }
+
+        public X509Certificate2? RetrieveCertificateForUser(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentException("Username cannot be null or empty", nameof(username));
+            }
+            username = username.Trim();
+
+            X509Store store = GetCertStore();
+            store.Open(OpenFlags.ReadOnly);
+
+            try
+            {
+                foreach (X509Certificate2 certificate in store.Certificates)
+                {
+                    if (certificate.SubjectName.Name != null && certificate.SubjectName.Name.Contains($"CN={username}", StringComparison.OrdinalIgnoreCase))
+                    {
+                        return certificate;
+                    }
+
+                    foreach (X509Extension extension in certificate.Extensions)
+                    {
+                        if (extension.Oid.Value.Equals("2.5.29.17")) //san
+                        {
+                            var asnEncodedData = new AsnEncodedData(extension.Oid, extension.RawData);
+                            string sanString = asnEncodedData.Format(false);
+                            if (sanString.Contains(username, StringComparison.OrdinalIgnoreCase))
+                            {
+                                return certificate;
+                            }
+                        }
+                    }
+                }
+
+                throw new InvalidOperationException($"Certificate for user {username} not found.");
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+    }
+}
