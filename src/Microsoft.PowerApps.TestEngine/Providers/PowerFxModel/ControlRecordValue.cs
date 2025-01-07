8c38c5a0c0b299492cca2924f16fbe38d3ce1a68
Nareshkumar Balasubramani <v-nabalasubr@microsoft.com>
2025-01-07 17:46:11 +0530

    Fixing white space issue

--- src/Microsoft.PowerApps.TestEngine/Providers/PowerFxModel/ControlRecordValue.cs
+++ src/Microsoft.PowerApps.TestEngine/Providers/PowerFxModel/ControlRecordValue.cs
@@ -111,7 +111,6 @@ namespace Microsoft.PowerApps.TestEngine.Providers.PowerFxModel
 
                 if (jsPropertyValueModel != null)
                 {
-                   
                     if (fieldType is NumberType)
                     {
                         result = NumberValue.New(double.Parse(jsPropertyValueModel.PropertyValue));
