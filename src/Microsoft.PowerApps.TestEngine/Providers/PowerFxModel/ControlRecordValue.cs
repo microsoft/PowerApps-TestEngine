7cf753c64df924f4fd2f0b478fe40c318567b3ff
v-raghulraja <v-raghulraja@microsoft.com>
2025-01-09 13:12:50 +0530

    Resolving review comments

--- src/Microsoft.PowerApps.TestEngine/Providers/PowerFxModel/ControlRecordValue.cs
+++ src/Microsoft.PowerApps.TestEngine/Providers/PowerFxModel/ControlRecordValue.cs
@@ -175,8 +175,7 @@ namespace Microsoft.PowerApps.TestEngine.Providers.PowerFxModel
                         if (long.TryParse(jsPropertyValueModel.PropertyValue, out milliseconds))
                         {
                             var dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(milliseconds);
-                            DateTime trueDateTime = dateTimeOffset.LocalDateTime;
-                            //var trueDateTime = new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds(milliseconds);
+                            DateTime trueDateTime = dateTimeOffset.LocalDateTime;                           
                             result = DateValue.NewDateOnly(trueDateTime.Date);
                         }
                         // When converted from DateTime to a string, a value from SetProperty() retains it's MMDDYYYY hh::mm::ss format
