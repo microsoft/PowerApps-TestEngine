870eebc20e6186a94e40df1cd47e57eebe274cc1
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2025-02-23 14:15:13 -0800

    PlaywrightScript path update

--- src/testengine.module.playwrightscript/PlaywrightScriptFunction.cs
+++ src/testengine.module.playwrightscript/PlaywrightScriptFunction.cs
@@ -41,6 +41,9 @@ namespace testengine.module
             _logger.LogInformation("------------------------------\n\n" +
                 "Executing PlaywrightScript function.");
 
+            // Convert relative path to path relativeto test file
+            var filename = GetFullFile(_testState, file.Value);
+
             if (!_filesystem.FileExists(file.Value))
             {
                 _logger.LogError("Invalid file");
@@ -48,8 +51,6 @@ namespace testengine.module
             }
 
             _logger.LogDebug("Loading file");
-
-            var filename = GetFullFile(_testState, file.Value);
             var script = _filesystem.ReadAllText(filename);
 
             var hash = ComputeSha256Hash(script);
